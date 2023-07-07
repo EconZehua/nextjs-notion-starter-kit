1d83c4ec45fc9fb28d2682f2fc08b63892be1cdd
Joshua <36633209+EconZehua@users.noreply.github.com>
2023-07-07 18:47:58 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '2469b41183b44ee2b59b9f8299507fb7',
+  rootNotionPageId: 'ae3df05687244b169e91484e3b1663f6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,16 +10,16 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'EconZehua',
-  domain: 'https://www.notion.so/yourbeloveddad/2469b41183b44ee2b59b9f8299507fb7',
+  domain: 'https://www.notion.so/yourbeloveddad/Homepage-ae3df05687244b169e91484e3b1663f6',
   author: 'Zehua Zhang Joshua',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Example Next.js Notion Starter',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '0xHank',
+  github: 'EconZehua',
+  linkedin: 'Zehua Zhang',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
